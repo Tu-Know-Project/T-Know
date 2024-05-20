84ac2ffe2542af019dfef9735f25cd66661f8f82
JoEunSang <sang11032@gmail.com>
2024-05-20 16:40:54 +0900

    [ui] mainPage NoActionbar 추가, 대표 아이콘 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,19 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
+// Load properties from the local.properties file
+val properties = Properties()
+properties.load(FileInputStream(rootProject.file("local.properties")))
+
 android {
-    viewBinding{enable=true}
+    viewBinding {
+        enable = true
+    }
     namespace = "com.example.t_know"
     compileSdk = 34
 
@@ -14,8 +23,9 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "CLIENT_KEY", properties.getProperty("CLIENT_KEY"))
     }
 
     buildTypes {
@@ -36,11 +46,11 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -50,10 +60,9 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("com.naver.maps:map-sdk:3.18.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
 }
--- app/src/main/java/com/example/t_know/mainPage/tapMenu/EatMenuFragment.kt
+++ app/src/main/java/com/example/t_know/mainPage/tapMenu/EatMenuFragment.kt
@@ -1,10 +1,12 @@
 package com.example.t_know.mainPage.tapMenu
 
 import BaseFragment
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.example.t_know.R
 import com.example.t_know.databinding.FragmentEatMenuBinding
+import com.example.t_know.mapPage.MapActivity
 
 class EatMenuFragment : BaseFragment<FragmentEatMenuBinding>(
     R.layout.fragment_eat_menu,
@@ -12,6 +14,9 @@ class EatMenuFragment : BaseFragment<FragmentEatMenuBinding>(
 ) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.btnMap.setOnClickListener{
+            val intent = Intent(activity, MapActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
